ae2869b8cff89755ac4155ff106ca2875da245dc
Swayam Pradeep Nakshane <nakshaneswayamatha24@gmail.com>
2025-08-03 11:21:44 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dashboard.arcapreit.com/",
+  baseURL: "https://dashboard.arcapreit.com/back/",
 });
 
 api.interceptors.request.use((config) => {
